a95077d4eb45961683fe7d6385c14f5aac26a699
frumania <info@frumania.com>
2019-01-20 12:59:14 +0100

    merge

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // index.js
 'use strict'
+
+//AWS XRAY
+var AWSXRay = require('aws-xray-sdk-core');
+var AWS = AWSXRay.captureAWS(require('aws-sdk'));
+AWSXRay.captureHTTPsGlobal(require('http'));
+var http = require('http');
+
 const awsServerlessExpress = require('aws-serverless-express')
 const app = require('./app')
 const server = awsServerlessExpress.createServer(app)
